e857aad5eeefeaf3e1bf801465a445724e2457dc
Cory Thomas <cthomas7577@gmail.com>
2023-05-04 13:02:31 -0500

    Seconds helper, supervisor scope, coroutine exception handler

--- src/main/kotlin/com/valbaca/gotlin/ch8/Clock2.kt
+++ src/main/kotlin/com/valbaca/gotlin/ch8/Clock2.kt
@@ -5,15 +5,16 @@ import java.net.ServerSocket
 import java.net.Socket
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import kotlin.time.DurationUnit.SECONDS
-import kotlin.time.toDuration
+import kotlin.time.Duration.Companion.seconds
 
 /*
 p222 NonBlocking TCP Clock Server using coroutines
 https://github.com/adonovan/gopl.io/tree/master/ch8/clock2
 */
 private val clockFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-fun main() = runBlocking(Dispatchers.IO) {
+private val printUncaughtException = CoroutineExceptionHandler { _, ex -> ex.printStackTrace() }
+
+suspend fun main() = supervisorScope {
     val listener = ServerSocket(8000)
     while (isActive) {
         try {
@@ -25,21 +26,15 @@ fun main() = runBlocking(Dispatchers.IO) {
     }
 }
 
-private fun CoroutineScope.handleConn(conn: Socket) = launch {
+private fun CoroutineScope.handleConn(conn: Socket) = launch(printUncaughtException) {
     conn.use {
         while (isActive) { // `isActive` instead of `true` is a good pattern
             val out = conn.getOutputStream()
             val formattedTime = clockFormatter.format(LocalTime.now()) + "\n"
             print(".")
-            try {
-                out.write(formattedTime.toByteArray())
-                out.flush()
-            } catch (e: Exception) {
-                cancel()
-                System.err.print(e)
-            }
-            delay(1.toDuration(SECONDS)) // Notice this is `delay` for the coroutine model
+            out.write(formattedTime.toByteArray())
+            out.flush()
+            delay(1.seconds) // Notice this is `delay` for the coroutine model
         }
     }
-
 }
