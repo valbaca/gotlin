841d919000251ab6ee7b5895e58db560e9fcf4fc
Valentin Baca <valbaca@gmail.com>
2023-05-12 11:19:15 -0700

    Finished 8.6 Concurrent Web Crawler

--- src/main/kotlin/com/valbaca/gotlin/ch8/Crawl3.kt
+++ src/main/kotlin/com/valbaca/gotlin/ch8/Crawl3.kt
@@ -7,40 +7,43 @@ import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-private val tokens = Channel<Any>(20)
-
 private suspend fun crawl(url: String): List<String> {
     println(url)
-    tokens.send(object {}) // acquire a token
     return try {
         extractLinks(url)
     } catch (e: Exception) {
         error(e)
-    } finally {
-        tokens.receive() // release the token
     }
 }
 
 fun main(args: Array<String>) = runBlocking(Dispatchers.IO) {
-    val worklist = Channel<List<String>>()
+    val worklist = Channel<List<String>>() // lists of URLs, may have duplicates
+    val unseenLinks = Channel<String>()    // de-duplicated URLs
 
     val start = listOf(args.ifEmpty { arrayOf("https://news.ycombinator.com/") }[0])
     launch { worklist.send(start) }
-    var n = 1 // n = # of pending sends to worklist
 
-    // Crawl the web concurrently (with concurrency bounds)
-    // ⚠️ WARNING! CRAWLER WILL LIKELY RUN FOREVER! ⚠️️
-    // TIP: add depth-limits
-    val seen = mutableSetOf<String>()
-    while (n > 0) {
-        n--
-        val list = worklist.receive()
-        for (link in list) {
-            if (link !in seen) {
-                seen.add(link)
-                n++
-                launch { worklist.send(crawl(link))}
+    repeat(20) {
+        launch {
+            unseenLinks.consumeEach {link ->
+                val foundLinks = crawl(link)
+                launch {
+                    worklist.send(foundLinks)
+                }
             }
         }
     }
+
+    // The main coroutine de-duplicates worklist items
+    // and sends the unseen ones to the crawlers
+    val seen = mutableSetOf<String>()
+    worklist.consumeEach { list ->
+        for (link in list) {
+            if (link !in seen) {
+                seen.add(link)
+                unseenLinks.send(link)
+            }
+        }
+    }
+    // For simplicity (and reflecting The Go book, does not address the termination problem)
 }
